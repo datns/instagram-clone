deb9dadba3ee61c5866d77ac1a8a7f31a2d939e8
datnguyen <dat.s.nguyen@taptap.com.vn>
2021-02-08 17:16:01 +0700

    setup firebase

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import * as firebase from "firebase";
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from "@react-navigation/stack";
 
 import LandingScreen from './components/auth/Landing';
 import RegisterScreen from './components/auth/Register';
-const Stack = createStackNavigator()
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBdbXRIEsqD4V2gH0xnvwAStVRjL6zZWkQ",
+    authDomain: "instagram-clone-f93ef.firebaseapp.com",
+    projectId: "instagram-clone-f93ef",
+    storageBucket: "instagram-clone-f93ef.appspot.com",
+    messagingSenderId: "974388182811",
+    appId: "1:974388182811:web:6a430609d42022adab1dbe",
+    measurementId: "G-DXWG6RSTKW"
+};
+
+if (firebase.apps.length === 0) {
+    firebase.initializeApp(firebaseConfig);
+}
+const Stack = createStackNavigator();
+
 export default function App() {
     return (
         <NavigationContainer>
