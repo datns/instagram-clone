2f16a56e7c77faa1d40dfb8a4aead87dd980e814
datnguyen <dat.s.nguyen@taptap.com.vn>
2021-02-11 11:19:03 +0700

    show info and posts in Profile

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,15 +1,54 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text, FlatList, StyleSheet, Image} from 'react-native';
 import {useSelector} from "react-redux";
-import store from '../../redux/store';
 
 export default function Profile() {
     const posts = useSelector(store => store.userState.posts);
     const currentUser = useSelector(store => store.userState.currentUser);
     console.log('posts', posts);
-    return(
-        <View>
-            <Text>Profile</Text>
+    return (
+        <View style={styles.container}>
+            <View style={styles.containerInfo}>
+                <Text>{currentUser.name}</Text>
+                <Text>{currentUser.email}</Text>
+            </View>
+            <View style={styles.containerGallery}>
+                <FlatList
+                    numColumns={3}
+                    horizontal={false}
+                    data={posts}
+                    renderItem={({item}) => {
+                        return (
+                            <View style={styles.containerImage}>
+                                <Image
+                                    style={styles.image}
+                                    source={{uri: item.downloadURL}}
+                                />
+                            </View>
+                        )
+                    }}
+                />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 40,
+    },
+    containerInfo: {
+        margin: 10
+    },
+    containerGallery: {
+        flex: 1,
+    },
+    containerImage: {
+        flex: 1 / 3,
+    },
+    image: {
+        flex: 1,
+        aspectRatio: 1
+    }
+});
