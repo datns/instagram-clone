5f35f48f5d7d12aeaf9bbc58e2a2f627ff84f27a
datnguyen <dat.s.nguyen@taptap.com.vn>
2021-02-11 08:54:05 +0700

    get image from device's gallery

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,17 +1,21 @@
 import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, Button, Image} from 'react-native';
 import {Camera} from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function Add() {
-    const [hasPermission, setHasPermission] = useState(null);
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
     const [camera, setCamera] = useState(null);
     const [image, setImage] = useState(null);
     const [type, setType] = useState(Camera.Constants.Type.back);
 
     useEffect(() => {
         (async () => {
-            const {status} = await Camera.requestPermissionsAsync();
-            setHasPermission(status === 'granted');
+            const { status: cameraStatus} = await Camera.requestPermissionsAsync();
+            setHasCameraPermission(cameraStatus === 'granted');
+            const { status: galleryStatus } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            setHasGalleryPermission(galleryStatus === 'granted');
         })();
     }, []);
 
@@ -21,10 +25,26 @@ export default function Add() {
             setImage(data.uri);
         }
     }
-    if (hasPermission === null) {
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
+    if (hasCameraPermission === null || hasGalleryPermission === null) {
         return <View/>;
     }
-    if (hasPermission === false) {
+    if (hasCameraPermission === false || hasGalleryPermission === false) {
         return <Text>No access to camera</Text>;
     }
     return (
@@ -47,6 +67,7 @@ export default function Add() {
                 }}>
             </Button>
             <Button title="Take Picture" onPress={takePicture}/>
+            <Button title="Pick Image From Gallery" onPress={pickImage}/>
             {image && <Image source={{uri: image}} style={{flex: 1}}/>}
         </View>
     );
